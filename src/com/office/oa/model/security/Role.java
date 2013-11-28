51eae72e60fb5b2b541f8b1668a461da3f631652
陶焕 <mail.taohuan@gmail.com>
2013-11-28 18:54:41 +0800

    Add Security Entitys

---
+++ src/com/office/oa/model/security/Role.java
@@ -0,0 +1,46 @@
+package com.office.oa.model.security;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import com.office.oa.model.EntityBase;
+
+/**
+ * 角色
+ * 
+ * @author huan.tao
+ * 
+ */
+@Entity
+@Table(name = "role")
+@Cache(usage = CacheConcurrencyStrategy.TRANSACTIONAL)
+@Getter
+@Setter
+public class Role extends EntityBase implements Serializable, Cloneable {
+
+	private static final long serialVersionUID = 2199269777825531301L;
+
+	@Column(name = "name", nullable = false, length = 32)
+	String name;
+
+	@Column(name = "no", nullable = false, length = 32)
+	String no = "";
+
+	@Column(name = "comment", nullable = true, length = 1000)
+	String comment;
+
+	@Column(name = "status", nullable = false)
+	Integer status = 0;
+
+}
---
+++ src/com/office/oa/security/authentication/details/SimpleUserDetail.java
@@ -0,0 +1,11 @@
+package com.office.oa.security.authentication.details;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.office.oa.model.security.User;
+
+public class SimpleUserDetail extends User implements UserDetails {
+
+	private static final long serialVersionUID = 458928384974276583L;
+
+}
